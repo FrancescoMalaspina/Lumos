7051d1ed24943f8586b064f9138886b5c83cb149
FrancescoMalaspina <francesco.malaspina04@gmail.com>
2024-01-20 20:05:19 +0100

    Add SymPy_PhotonicMolecule class and corresponding webapp page

---
+++ pages/Photonic_Molecule.py
@@ -0,0 +1,54 @@
+# third party imports
+import streamlit as st
+import numpy as np
+import plotly.graph_objects as go
+from scipy.constants import c
+import matplotlib.pyplot as plt
+
+# local imports
+from src.sympy.photonic_molecule import SymPy_PhotonicMolecule
+from src.sympy.ring_resonator import SymPy_RingResonator
+
+PM = SymPy_PhotonicMolecule()
+ring = SymPy_RingResonator()
+
+st.set_page_config(layout='wide')
+st.title('Interferometric Coupler (DCDR)')
+
+pin = st.sidebar.selectbox('Pin', [0, 1, 2, 3, 4, 5, 6, 7], index=1)
+# onluy integer values are allowed for m, n, k, min_value=1, step=1
+m_1 = st.sidebar.number_input('m', value=1, min_value=1, step=1)
+m_2 = st.sidebar.number_input('n', value=1, min_value=1, step=1)
+p = st.sidebar.number_input('p', value=1, min_value=1, step=1)
+cross_coupling_1 = st.sidebar.number_input('cross_coupling_1', value=0.5, min_value=0., max_value=1., step=0.01)
+cross_coupling_2 = st.sidebar.number_input('cross_coupling_2', value=0.48, min_value=0., max_value=1., step=0.01)
+unitary_loss_coefficient = st.sidebar.number_input('unitary_loss_coefficient', value=0.999, min_value=0., max_value=1.,  format='%.3f')
+
+pm_params = {
+    'm_1': m_1,
+    'm_2': m_2,
+    'p': p,
+    'cross_coupling_1': cross_coupling_1,
+    'unitary_loss_coefficient': unitary_loss_coefficient,
+    "cross_coupling_2": cross_coupling_2,
+    'self_coupling_1': np.sqrt(1 - cross_coupling_1**2),
+    'self_coupling_2': np.sqrt(1 - cross_coupling_2**2),
+}
+
+ring_params = {
+    'l': m_1+m_2,
+    'cross_coupling_1': cross_coupling_1,
+    'self_coupling_1': np.sqrt(1 - cross_coupling_1**2),
+    'unitary_loss_coefficient': unitary_loss_coefficient,
+}
+
+PM.numeric_parameters = pm_params
+ring.numeric_parameters = ring_params
+
+fig, ax = plt.subplots(1, 2, figsize = (20, 9))
+PM.pole_zero_plot(pin=1, ax=ax[0], fig=fig)
+PM.magnitude_response_plot(pin=pin, ax=ax[1], fig=fig, label='DCDR', omega=np.linspace(-np.pi, np.pi, 10000))
+ring.magnitude_response_plot(pin=pin, ax=ax[1], fig=fig, is_reference=True, omega=np.linspace(-np.pi, np.pi, 10000))
+# plt.show()
+
+st.pyplot(fig)
---
+++ src/sympy/photonic_molecule.py
@@ -0,0 +1,41 @@
+from src.sympy.base import SymPy_PhotonicCircuit
+
+from sympy import symbols, Eq, I, linsolve, together, lambdify
+from sympy.physics.control.lti import TransferFunction
+from src.sympy.utils import pole_zero_plot
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SymPy_PhotonicMolecule(SymPy_PhotonicCircuit):
+    num_pins = 8
+
+    def __init__(self, numeric_parameters=None):
+        super().__init__(numeric_parameters=numeric_parameters)
+    
+    @property
+    def parameter_symbols(self):
+        return {
+            "m_1": symbols("m_1"),
+            "m_2": symbols("m_2"),
+            "p": symbols("p"),
+            "cross_coupling_1": symbols(r"\kappa_1"),
+            "cross_coupling_2": symbols(r"\kappa_2"),
+            "self_coupling_1": symbols(r"\sigma_1"),
+            "self_coupling_2": symbols(r"\sigma_2"),
+            "unitary_loss_coefficient": symbols(r"\gamma"),
+        }
+    
+    @property
+    def equations(self):
+        a0, a1, a2, a3, a4, a5, a6, a7 = self.pins
+        z = self.z
+        return [
+            Eq(a0, 1),
+            Eq(a2, self.parameter_symbols["self_coupling_1"] * a0 + I * self.parameter_symbols["cross_coupling_1"] * a1),
+            Eq(a3, I * self.parameter_symbols["cross_coupling_1"] * a0 + self.parameter_symbols["self_coupling_1"] * a1),
+            Eq(a6, self.parameter_symbols["self_coupling_2"] * a4 + I * self.parameter_symbols["cross_coupling_2"] * a5),
+            Eq(a7, I * self.parameter_symbols["cross_coupling_2"] * a4 + self.parameter_symbols["self_coupling_2"] * a5),
+            Eq(a5, a3 * (self.parameter_symbols["unitary_loss_coefficient"] * z ** (-1)) ** self.parameter_symbols["m_2"]),
+            Eq(a1, a7 * (self.parameter_symbols["unitary_loss_coefficient"] * z ** (-1)) ** self.parameter_symbols["m_1"]),
+            Eq(a4, a6 * (self.parameter_symbols["unitary_loss_coefficient"] * z ** (-1)) ** self.parameter_symbols["p"]),
+        ]
